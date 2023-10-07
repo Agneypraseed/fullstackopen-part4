c0fe4d91b0ac48b65cb0aa6b65060c754aaca69f
Agney Praseed <agneysince2000@gmail.com>
2023-10-08 01:40:00 +0530

    4.6*: helper functions and unit tests, step4 - Without Lodash

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -17,8 +17,30 @@ const favoriteBlog = (blogs) => {
       }, blogs[0]);
 };
 
+const mostBlogs = (blogs) => {
+  const namestoCountMap = [];
+  blogs.forEach((item) => {
+    if (item.author) {
+      const existingName = namestoCountMap.find(
+        (entry) => entry.author === item.author
+      );
+      if (existingName) {
+        existingName.blogs += 1;
+      } else {
+        namestoCountMap.push({ author: item.author, blogs: 1 });
+      }
+    }
+  });
+  return namestoCountMap.length === 0
+    ? null
+    : namestoCountMap.reduce((max, current) => {
+        return current.blogs > max.blogs ? current : max;
+      }, namestoCountMap[0]);
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 };
