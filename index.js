e0f00e57046a4fae154b2db7a8c6af90b8cc4c51
Agney Praseed <agneysince2000@gmail.com>
2023-10-07 15:05:55 +0530

    4.2 Blog list, step2 - refactored endpoints to seperate module

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const config = require("./utils/config");
 const logger = require("./utils/logger");
-const Blog = require("./models/blog");
 const express = require("express");
 const app = express();
+const blogsRouter = require("./controllers/blogController");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
@@ -19,20 +19,7 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use("/api/blogs", blogsRouter);
 
 const PORT = config.PORT || 3003;
 app.listen(PORT, () => {
