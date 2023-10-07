ebd8f412d01e10241f136220fcc41d80449645ec
Agney Praseed <agneysince2000@gmail.com>
2023-10-07 14:29:00 +0530

    4.2 Blog list, step2 - refactored collection to seperate module

--- index.js
+++ index.js
@@ -1,34 +1,20 @@
 const config = require("./utils/config");
+const logger = require("./utils/logger");
+const Blog = require("./models/blog");
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-blogSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
-
-console.log(`connecting to ${config.MONGODB_URI}`);
+mongoose.set("strictQuery", false);
+logger.info(`connecting to ${config.MONGODB_URI}`);
 mongoose
   .connect(config.MONGODB_URI)
   .then(() => {
-    console.log("connected to MongoDB");
+    logger.info("connected to MongoDB");
   })
   .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
+    logger.info("error connecting to MongoDB:", error.message);
   });
 
 app.use(cors());
@@ -50,5 +36,5 @@ app.post("/api/blogs", (request, response) => {
 
 const PORT = config.PORT || 3003;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  logger.info(`Server running on port ${PORT}`);
 });
