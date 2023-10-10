6ed06e586d72e5585736c9a3a61996729e935d77
Agney Praseed <agneysince2000@gmail.com>
2023-10-11 01:20:41 +0530

    4.8: Blog list tests, step1 - test for get api

---
+++ tests/blog_list_api.test.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+const Blog = require("../models/blog");
+const helper = require("./test_helper");
+
+const api = supertest(app);
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+
+  for (let blog of helper.initialBLogList) {
+    let blogObject = new Blog(blog);
+    await blogObject.save();
+  }
+});
+
+test("the blog list application returns the correct amount of blog posts", async () => {
+  const response = await api.get("/api/blogs");
+
+  expect(response.body).toHaveLength(6);
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+});
