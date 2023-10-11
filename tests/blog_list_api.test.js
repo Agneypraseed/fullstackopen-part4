eb27ec08073ba9637814895a6ad84224c4c59133
Agney Praseed <agneysince2000@gmail.com>
2023-10-11 18:51:20 +0530

    4.9: Blog list tests, step2

--- tests/blog_list_api.test.js
+++ tests/blog_list_api.test.js
@@ -21,11 +21,11 @@ test("the blog list application returns the correct amount of blog posts", async
   expect(response.body).toHaveLength(6);
 });
 
-test("the blog list application returns the correct amount of blog posts", async () => {
-    const response = await api.get("/api/blogs");
+test("the unique identifier property of the blog posts is named id", async () => {
+  const response = await api.get("/api/blogs");
   
-    expect(response.body).toHaveLength(6);
-  });
+  expect(response.body[0].id).toBeDefined();
+});
 
 afterAll(async () => {
   await mongoose.connection.close();
