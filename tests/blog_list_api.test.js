954906d8134aae0f688943ea79739f7ef3990fd7
Agney Praseed <agneysince2000@gmail.com>
2023-10-11 19:52:04 +0530

    4.10: Blog list tests, step3

--- tests/blog_list_api.test.js
+++ tests/blog_list_api.test.js
@@ -18,13 +18,43 @@ beforeEach(async () => {
 test("the blog list application returns the correct amount of blog posts", async () => {
   const response = await api.get("/api/blogs");
 
-  expect(response.body).toHaveLength(6);
+  expect(response.body).toHaveLength(helper.initialBLogList.length);
 });
 
 test("the unique identifier property of the blog posts is named id", async () => {
   const response = await api.get("/api/blogs");
-  
-  expect(response.body[0].id).toBeDefined();
+
+  response.body.forEach((blog) => {
+    expect(blog.id).toBeDefined();
+  });
+});
+
+describe("Adding a new Blog", () => {
+  let newBlogList;
+  const newBlog = {
+    title: "Test Blog",
+    author: "Agney Chan",
+    url: "https://reactpatterns.com/",
+    likes: 100,
+  };
+
+  beforeAll(async () => {
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+    newBlogList = await helper.blogsInDb();
+  });
+
+  test("creating a new blog post using post request makes the total blog to increase by 1", async () => {
+    expect(newBlogList).toHaveLength(helper.initialBLogList.length + 1);
+  });
+
+  test("creating a new blog post adds its content correctly to the database.", async () => {
+    const blogTitles = newBlogList.map((n) => n.title);
+    expect(blogTitles).toContain("Test Blog");
+  });
 });
 
 afterAll(async () => {
--- tests/test_helper.js
+++ tests/test_helper.js
@@ -1,3 +1,5 @@
+const Blog = require("../models/blog");
+
 const initialBLogList = [
   {
     _id: "5a422a851b54a676234d17f7",
@@ -49,4 +51,9 @@ const initialBLogList = [
   },
 ];
 
-module.exports = { initialBLogList };
+const blogsInDb = async () => {
+  const blogs = await Blog.find({});
+  return blogs.map((blog) => blog.toJSON());
+};
+
+module.exports = { initialBLogList, blogsInDb };
