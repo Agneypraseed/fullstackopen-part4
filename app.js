8481993f6d0c91e92471468ed6f89f334d28dd4f
Agney Praseed <agneysince2000@gmail.com>
2023-10-13 00:31:09 +0530

    4.15: bloglist expansion, step3

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const app = express();
 const middleware = require("./utils/errorHandler");
 const blogsRouter = require("./controllers/blogController");
+const usersRouter = require("./controllers/userContoller");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
@@ -21,6 +22,7 @@ mongoose
 app.use(cors());
 app.use(express.json());
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 app.use(middleware.errorHandler);
 
 module.exports = app;
