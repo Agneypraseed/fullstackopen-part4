95039365e4df6cd85bfced485563e897b9c2dfe4
Agney Praseed <agneysince2000@gmail.com>
2023-10-13 12:53:34 +0530

    4.16: bloglist expansion, step4 - error message added

--- controllers/userContoller.js
+++ controllers/userContoller.js
@@ -1,26 +1,34 @@
 const bcrypt = require("bcrypt");
 const usersRouter = require("express").Router();
 const User = require("../models/user");
+const { error } = require("../utils/logger");
 
 usersRouter.get("/", async (request, response) => {
   const users = await User.find({});
   response.json(users);
 });
 
-usersRouter.post("/", async (request, response) => {
-  const { username, name, password } = request.body;
+usersRouter.post("/", async (request, response, next) => {
+  try {
+    const { username, name, password } = request.body;
 
-  const saltRounds = 10;
-  const passwordHash = await bcrypt.hash(password, saltRounds);
+    if (!password)
+      return response.status(400).json({ error: "Password cannot empty" });
 
-  const user = new User({
-    username,
-    name,
-    password: passwordHash,
-  });
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds);
 
-  const savedUser = await user.save();
-  response.status(201).json(savedUser);
+    const user = new User({
+      username,
+      name,
+      password: passwordHash,
+    });
+
+    const savedUser = await user.save();
+    response.status(201).json(savedUser);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = usersRouter;
--- models/user.js
+++ models/user.js
@@ -1,11 +1,21 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const userSchema = new mongoose.Schema({
-  username: String,
+  username: {
+    type: String,
+    required: true,
+    minLength: 3,
+    unique: true,
+  },
   name: String,
-  password: String,
+  password: {
+    type: String,
+    required: true,
+    minLength: 3,
+  },
 });
-
+userSchema.plugin(uniqueValidator);
 userSchema.set("toJSON", {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
